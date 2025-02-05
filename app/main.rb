4244cfdeb11b148fdc7eddf6a0151fd1280bad53
tantrungse <tantrungse@gmail.com>
2025-02-05 23:51:00 +0700

    The exit built-in

--- app/main.rb
+++ app/main.rb
@@ -1,5 +1,8 @@
-while true
+command_list = ['exit']
+command = ''
+
+while command != command_list[0]
   $stdout.write("$ ")
   command, *args = gets.chomp.split(" ")
-  $stdout.write("#{command}: command not found\n")  
+  $stdout.write("#{command}: command not found\n") unless command == command_list[0]
 end
