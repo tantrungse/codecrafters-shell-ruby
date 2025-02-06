793b48dcebd926e26823c29b71a229062f96e135
tantrungse <tantrungse@gmail.com>
2025-02-06 14:15:08 +0700

    codecrafters submit [skip ci]

--- app/main.rb
+++ app/main.rb
@@ -1,6 +1,7 @@
 cmd_list = {
   exit_cmd: 'exit',
-  echo_cmd: 'echo'
+  echo_cmd: 'echo',
+  type_cmd: 'type'
 }
 cmd = ''
 
@@ -15,6 +16,12 @@ while cmd != cmd_list[0]
     break
   when cmd_list[:echo_cmd]
     $stdout.write("#{args.join(' ')}\n")
+  when cmd_list[:type_cmd]
+    if cmd_list.has_value?(args[0])
+      $stdout.write("#{args[0]} is a shell builtin\n")
+    else
+      $stdout.write("#{args[0]}: not found\n")
+    end
   else
     $stdout.write("#{cmd}: command not found\n")
   end
